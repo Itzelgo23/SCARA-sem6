d193ac996d221ca5f75ed4dcb73c615782cabf70
Itzel Gonzalez <A01781429@tec.mx>
2026-06-02 18:08:19 -0600

    Edit on PickandPlace Library to move base up before operation

--- src/PickandPlace.h
+++ src/PickandPlace.h
@@ -8,6 +8,7 @@ bool setPick(float pick_coords1,float pick_coords2,float pick_coords3)
     bool Pick = false;
     float location[3] = {pick_coords1, pick_coords2, pick_coords3};
     Gripper_Motor.set(0);
+    angle_AS5600 = cm2deg(5, 8.0f);
     PIDmotors(angle_AS5600, Base, control[0], error[0], angle_S[0], speed_S[0]);
     getIK(location, L1, L2, num_solutions, solutions);
     PIDmotors(solutions[0][0], Shoulder, control[1], error[1], angle_S[1], speed_S[1]);
@@ -27,6 +28,7 @@ bool setPick(float pick_coords1,float pick_coords2,float pick_coords3)
 bool setPlace(float place_coords[3])
 {
     bool Place = false;
+    angle_AS5600 = cm2deg(5, 8.0f);
     PIDmotors(angle_AS5600, Base, control[0], error[0], angle_S[0], speed_S[0]);
     getIK(place_coords, L1, L2, num_solutions, solutions);
     PIDmotors(solutions[0][0], Shoulder, control[1], error[1], angle_S[1], speed_S[1]);
